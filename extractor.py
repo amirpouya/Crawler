054abfeab3b5c723bf3b8340d05813ba8bbba246
amirpouya <amirpouya.a@gmail.com>
2015-03-10 08:29:59 +0330

    Write to xml file

--- extractor.py
+++ extractor.py
@@ -11,8 +11,8 @@ import io
 
 
 
-
 def main():
+    counter=0
     list_file=open("final.list.txt",'r')
     links=list_file.readlines()
     counter=0;
@@ -31,23 +31,39 @@ def main():
             trg_html=trg_html.replace('item_tab_contents_wrapper ','')
             #targ_file=open(file_name,'w')
             outdir="output"
-            text_extract(trg_html,org_html,outdir)
+            counter=text_extract(trg_html,link2,org_html,link,outdir,counter)
             #targ_file.write(str(targ_html))
             #targ_file.close()
         except   :
             print "could not download %s"%link
 
 
-def text_extract(trg_html,org_html,outdir):
-    trg_soup = BeautifulSoup(''.join(trg_html))
-    org_soup = BeautifulSoup(''.join(org_html))
+def text_extract(trg_html,trg_link,org_html,org_link,outdir,counter):
+
+    trg_out=unicode('<doc url="'+trg_link.strip()+'">')
+    org_out=unicode('<doc url="'+org_link.strip()+'">')
+
+
+
+    trg_soup = BeautifulSoup(''.join(trg_html),convertEntities=BeautifulSoup.HTML_ENTITIES)
+    org_soup = BeautifulSoup(''.join(org_html),convertEntities=BeautifulSoup.HTML_ENTITIES)
 
     trg_part={}
     org_part={}
     trg_title=trg_soup.findAll('h1',{'class':'item-title'})
     #print trg_title
     org_title=org_soup.findAll('h1',{'class':'item-title'})
+    if len(trg_title)<1 or len(org_title)<1:
+        return counter
 
+    #Extracting and Generating XML docs
+
+    #Title Part
+    trg_out+=unicode("<title>"+trg_title[0].text+"</title>")
+    org_out+=unicode("<title>"+org_title[0].text+"</title>")
+
+
+    #Exetracting Diffrent type of Desc
     trg_desc=trg_soup.findAll('div',{'class':'itemDescription'})
     org_desc=org_soup.findAll('div',{'class':'itemDescription'})
 
@@ -66,17 +82,35 @@ def text_extract(trg_html,org_html,outdir):
     trg_part['item-desc']=trg_desc
     org_part['item-desc']=org_desc
 
+    print trg_title[0].text
+    for item in trg_part.keys():
+        if len(trg_part[item])>0:
+            trg_out+=unicode('<desc type="'+item+'">'+trg_part[item][0].text+"</desc>")
+    print org_title[0].text
+    for item in org_part.keys():
+        if len(org_part[item])>0:
+            org_out+=unicode('<desc type="'+item+'">'+org_part[item][0].text+"</desc>")
+    trg_out+=unicode("</doc>")
+    org_out+=unicode("</doc>")
+    counter+=1;
 
+    if not os.path.exists(outdir):
+        os.makedirs(outdir)
+    if not os.path.exists(outdir+"\\en"):
+        os.makedirs(outdir+"\\en")
+    if not os.path.exists(outdir+"\\ar"):
+        os.makedirs(outdir+'\\ar')
 
-    if len(trg_title)>0:
-        print trg_title[0].text
-        for item in trg_part.keys():
-            if len(trg_part[item])>0:
-                print item,trg_part[item][0].text
-        print org_title[0].text
-        for item in org_part.keys():
-            if len(org_part[item])>0:
-                print item,org_part[item][0].text
+    org_file=io.open(outdir+"\\en\\"+str(counter)+".en",'w',encoding='utf-8')
+    trg_file=io.open(outdir+"\\ar\\"+str(counter)+".ar",'w',encoding='utf-8')
+
+    org_file.write(org_out)
+    trg_file.write(trg_out)
+    org_file.close()
+    trg_file.close()
+
+    print "--------------------------"
+    return counter
 
 
 
