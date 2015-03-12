25c1cbf108c80df04e598ad0adeccc15da363aaf
amirpouya <amirpouya.a@gmail.com>
2015-03-12 20:41:05 +0330

    Adding Arabic Recognizer

--- extractor.py
+++ extractor.py
@@ -1,3 +1,6 @@
+#!/usr/bin/env python
+# -*- coding: UTF-8 -*-
+
 #usage: python extractor.py -i<listfile> -o<outdir>
 # or
 # python extractor.py
@@ -11,6 +14,7 @@ import sys,getopt
 
 
 def main(argv):
+    fcounter=0
     list_address='final.list.txt'
     output_dir='output'
 
@@ -39,6 +43,7 @@ def main(argv):
     all_file_count=len(links)
     print "Start Downloading Pages..."
     for link in links:
+        fcounter+=1
         try:
             link2=link.replace('ae-en','ae-ar')
             print link,link2
@@ -51,13 +56,13 @@ def main(argv):
             trg_html=trg_html.replace('item_tab_contents_wrapper ','')
             outdir=output_dir
             counter=text_extract(trg_html,link2,org_html,link,outdir,counter)
-            print counter,'/',all_file_count
+            print fcounter,'/',all_file_count,":",counter
         except   :
             print "could not download %s"%link
 
 
 def text_extract(trg_html,trg_link,org_html,org_link,out_dir,counter):
-
+    ok=False
     trg_out=unicode('<doc url="'+trg_link.strip()+'">')
     org_out=unicode('<doc url="'+org_link.strip()+'">')
 
@@ -90,53 +95,61 @@ def text_extract(trg_html,trg_link,org_html,org_link,out_dir,counter):
     trg_desc=trg_soup.findAll('li',{'id':'Description'})
     org_desc=org_soup.findAll('li',{'id':'Description'})
 
-    trg_part['Description']=trg_desc
-    org_part['Description']=org_desc
+    if isArabic(trg_desc):
+        trg_part['Description']=trg_desc
+        org_part['Description']=org_desc
 
     trg_desc=trg_soup.findAll('div',{'class':'item-desc'})
     org_desc=org_soup.findAll('div',{'class':'item-desc'})
 
-    trg_part['item-desc']=trg_desc
-    org_part['item-desc']=org_desc
+    if isArabic(trg_desc):
+        trg_part['item-desc']=trg_desc
+        org_part['item-desc']=org_desc
 
     trg_desc=trg_soup.findAll('div',{'class':'product_text'})
     org_desc=org_soup.findAll('div',{'class':'product_text'})
-    trg_part['product_text']=trg_desc
-    org_part['product_text']=org_desc
-    print trg_title[0].text
-    for item in trg_part.keys():
-        if len(trg_part[item])>0:
-            trg_out+=unicode('<desc type="'+item+'">\n'+trg_part[item][0].text.replace('ENDHERE','\n').replace('ENDSTART','\n').replace('END','\n')+"</desc>\n")
-    print org_title[0].text
-    for item in org_part.keys():
-        if len(org_part[item])>0:
-            org_out+=unicode('<desc type="'+item+'">\n'+org_part[item][0].text.replace('ENDHERE','\n').replace('ENDSTART','\n').replace('END','\n')+"</desc>\n")
-    trg_out+=unicode("</doc>")
-    org_out+=unicode("</doc>")
-    en_path=os.path.join(out_dir,'en')
-    ar_path=os.path.join(out_dir,'ar')
-    if not os.path.exists(out_dir):
-        os.makedirs(out_dir)
-    if not os.path.exists(en_path):
-        os.makedirs(en_path)
-    if not os.path.exists(ar_path):
-        os.makedirs(ar_path)
+    if isArabic(trg_desc):
+        trg_part['product_text']=trg_desc
+        org_part['product_text']=org_desc
+    if ok:
+        print trg_title[0].text
+        for item in trg_part.keys():
+            if len(trg_part[item])>0:
+                trg_out+=unicode('<desc type="'+item+'">\n'+trg_part[item][0].text.replace('ENDHERE','\n').replace('ENDSTART','\n').replace('END','\n')+"</desc>\n")
+        print org_title[0].text
+        for item in org_part.keys():
+            if len(org_part[item])>0:
+                org_out+=unicode('<desc type="'+item+'">\n'+org_part[item][0].text.replace('ENDHERE','\n').replace('ENDSTART','\n').replace('END','\n')+"</desc>\n")
+        trg_out+=unicode("</doc>")
+        org_out+=unicode("</doc>")
+        en_path=os.path.join(out_dir,'en')
+        ar_path=os.path.join(out_dir,'ar')
+        if not os.path.exists(out_dir):
+            os.makedirs(out_dir)
+        if not os.path.exists(en_path):
+            os.makedirs(en_path)
+        if not os.path.exists(ar_path):
+            os.makedirs(ar_path)
 
-    org_path=os.path.join(en_path,str(counter)+".en")
-    trg_path=os.path.join(ar_path,str(counter)+".ar")
-    org_file=io.open(org_path,'w',encoding='utf-8')
-    trg_file=io.open(trg_path,'w',encoding='utf-8')
-    #org_out=BeautifulSoup(org_out)
-    #trg_out=BeautifulSoup(trg_out)
-    #org_out=unicode(org_out.prettify(),errors='replace')
-    #trg_out=unicode(trg_out.prettify(),errors='replace')
-    org_file.write(org_out)
-    trg_file.write(trg_out)
-    org_file.close()
-    trg_file.close()
+        org_path=os.path.join(en_path,str(counter)+".en")
+        trg_path=os.path.join(ar_path,str(counter)+".ar")
+        org_file=io.open(org_path,'w',encoding='utf-8')
+        trg_file=io.open(trg_path,'w',encoding='utf-8')
+        org_file.write(org_out)
+        trg_file.write(trg_out)
+        org_file.close()
+        trg_file.close()
 
-    counter+=1
+        counter+=1
     return counter
+
+
+def isArabic(seq):
+    aset=['ا','ب','ت','ث','ي','ي','ن','م','ل','ك','ق','ف','غ','ع','ظ','س']
+    """ Check whether sequence seq contains ANY of the items in aset. """
+    for c in seq:
+        if c in aset: return True
+    return False
 
 
 
